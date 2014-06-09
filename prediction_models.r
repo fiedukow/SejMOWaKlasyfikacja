b3dd58926b731c8e9f11eef79c65b3efcea05eb4
Lukasz <lukaszpetlinski@gmail.com>
2014-06-09 22:16:33 +0200

    Dodane inne modele drzew decyzyjnych:
    + Jeden z wyborem zmiennej jak w lesie drzew decyzyjnych
    + Drugi z możliwością większego podziału drzewa
    + Trzeci z możliwością użycia innych parametrów do podziału

--- prediction_models.r
+++ prediction_models.r
@@ -27,33 +27,69 @@ treePrediction = function(data_build, data_testing) {
   predict(decisionTree, newdata = data_testing)
 }
 
+treePredictionRandomForestVariableSelection = function(data_build, data_testing) {
+  decisionTree = ctree(party_changed ~ own_dst +
+                                       own_mode_dst +
+                                       own_dst_norm +
+                                       avg_streak_against_party +
+                                       max_streak_against_party,
+                       data = data_build, control = ctree_control(mtry = 1))
+  predict(decisionTree, newdata = data_testing)
+}
+
+treePredictionMoreSplit = function(data_build, data_testing) {
+  decisionTree = ctree(party_changed ~ own_dst +
+                                       own_mode_dst +
+                                       own_dst_norm +
+                                       avg_streak_against_party +
+                                       max_streak_against_party,
+                       data = data_build, control = ctree_control(minsplit = 5, minbucket = 2))
+  predict(decisionTree, newdata = data_testing)
+}
+
+treePredictionLessThresholdToSplit = function(data_build, data_testing) {
+  decisionTree = ctree(party_changed ~ own_dst +
+                                       own_mode_dst +
+                                       own_dst_norm +
+                                       avg_streak_against_party +
+                                       max_streak_against_party,
+                       data = data_build, control = ctree_control(mincriterion = 0.2, testtype = "Univariate", mtry=1))
+  predict(decisionTree, newdata = data_testing)
+}
+
 tresholdPrediction = function(data_build, data_testing, statistic) {
   data_testing[,statistic]
 }
 
 PredictAndDrawAll = function(DATA, filename = "tmp", ...) {
-  allPredictions = matrix(c(0), nrow = 7, ncol=length(DATA[[2]][,"party_changed"]))
+  allPredictions = matrix(c(0), nrow = 10, ncol=length(DATA[[2]][,"party_changed"]))
   expected = DATA[[2]][,"party_changed"]
 
   allPredictions[1,] = bayesPrediction(DATA[[1]], DATA[[2]])
   allPredictions[2,] = treePrediction(DATA[[1]], DATA[[2]])
-  allPredictions[3,] = tresholdPrediction(DATA[[1]], DATA[[2]], "own_dst")
-  allPredictions[4,] = tresholdPrediction(DATA[[1]], DATA[[2]], "own_dst_norm")
-  allPredictions[5,] = tresholdPrediction(DATA[[1]], DATA[[2]], "own_mode_dst")
-  allPredictions[6,] = tresholdPrediction(DATA[[1]], DATA[[2]], "avg_streak_against_party")
-  allPredictions[7,] = tresholdPrediction(DATA[[1]], DATA[[2]], "max_streak_against_party")
-  colors = c("red","green","blue","yellow","brown","black","orange")
+  allPredictions[3,] = treePredictionRandomForestVariableSelection(DATA[[1]], DATA[[2]])
+  allPredictions[4,] = treePredictionMoreSplit(DATA[[1]], DATA[[2]])
+  allPredictions[5,] = treePredictionLessThresholdToSplit(DATA[[1]], DATA[[2]])
+  allPredictions[6,] = tresholdPrediction(DATA[[1]], DATA[[2]], "own_dst")
+  allPredictions[7,] = tresholdPrediction(DATA[[1]], DATA[[2]], "own_dst_norm")
+  allPredictions[8,] = tresholdPrediction(DATA[[1]], DATA[[2]], "own_mode_dst")
+  allPredictions[9,] = tresholdPrediction(DATA[[1]], DATA[[2]], "avg_streak_against_party")
+  allPredictions[10,] = tresholdPrediction(DATA[[1]], DATA[[2]], "max_streak_against_party")
+  colors = c("red","green","blue","yellow","brown","black","orange","pink","gray","darkgreen")
 
   png(filename = paste("./plots/", filename, ".png", sep=""),
       width = 852, height = 711, units = "px", pointsize = 16,
       bg = "white")
-  for (i in 1:7) {
+  for (i in 1:10) {
     draw_roc_curve(allPredictions[i,], expected, add=(i>1), lwd=3, col=colors[i], ...)
   }
 
-  legend(x=0.465, y=0.3,
+  legend(x=0.465, y=0.4,
          legend = c("Naive Bayes",
                     "Decisive Tree",
+                    "Decisive Tree - random forest variable selction",
+                    "Decision Tree - more split",
+                    "Decision Tree - less threshold to split",
                     "Treshold - party vote dst.",
                     "Treshold - party vote dst. norm.",
                     "Treshold - party vote mode dst.",
